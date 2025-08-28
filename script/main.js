88359605fdb1b3579c0884b9c2f85e50196df3dd
Guilherme Garcia <guigarciamonteiro@gmail.com>
2025-08-27 22:58:08 -0300

    Add mensagem of error, Style works in 350px and implement localStorage

--- script/main.js
+++ script/main.js
@@ -4,12 +4,20 @@ const taskList = document.querySelector("[data-js-task-list]");
 const deleteTasksButton = document.querySelector("[data-js-delete-tasks-button]");
 const remainingTasks = document.querySelector("[data-js-remaining-tasks]");
 
-let taskNumber = 1;
-
 taskButton.addEventListener("click",AddTask);
 taskButton.addEventListener("keydown", (event) => {if (event.key === 'Enter' || event.key === ' '){AddTask()}});
 taskInput.addEventListener("keydown", (event) => {if (event.key === 'Enter') {AddTask()}});
+taskInput.addEventListener("input", clearErr);
 deleteTasksButton.addEventListener("click",RemoveAllTasks);
+
+let taskNumber = 1;
+var tasks = [];
+
+//localStorage.removeItem("tasks"); CASO SEJA NECESSÁRIO APAGAR AS TAREFAS, APENAS COLE ESSE COMANDO!
+
+if(localStorage.getItem("tasks") != undefined){
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+}
 
 function clearInput(){
     taskInput.value = ""
@@ -18,16 +26,26 @@ function clearInput(){
 function AddTask() {
     const taskText = taskInput.value.trim();
 
-    if(taskText == "" || taskNumber == 11) return;
+    if(taskText == ""){
+        errorTask("Não pode adicionar tarefas vazias");
+        return;
+    }
+
+    if(taskNumber == 11){
+        errorTask("Não pode adicionar mais que 10 tarefas!");
+        return;
+    }
+
+    clearErr();
     clearInput();
 
     let novaTarefa = document.createElement("li");
-    novaTarefa.id = `task_${taskNumber}`;
+    novaTarefa.id = `${taskNumber}`;
     novaTarefa.className = "main_list__task";
     novaTarefa.innerHTML =  `
                             <label class="main_list_task__label">
                                 <input class="label__do" type="checkbox" data-js-checkbox>
-                                <span class="label__text_task">${taskText}</span>
+                                <span class="label__text_task">${taskText[0].toUpperCase() + taskText.substring(1,taskText.length)}</span>
                             </label>
 
                             <button id="${taskNumber}" class="delete_task" aria-label="Delete task" data-js-delete-task-button>
@@ -45,28 +63,42 @@ function AddTask() {
     );
 
     remainingTasks.textContent = `Your remaining todos: ${taskList.childElementCount}`
+    
+    tasks.push({
+        texto: taskText,
+        checked: false
+    });
+
+    //addInLocalStorage(tasks);
 
     UpdateRemainingTasks()
-
     taskNumber++;
 }
 
 function RemoveTask(task) {
     task.remove();
+    tasks.slice(task.id, task.id)
+    //addInLocalStorage(tasks);
+
     taskNumber--;
     UpdateRemainingTasks()
 }
 
-function RemoveAllTasks(){
-    if(taskList.hasChildNodes()){
-        taskNumber = 1;
-        
-        while(taskList.lastChild){
-            taskList.lastChild.remove()
-        }
-
-        UpdateRemainingTasks()
+function RemoveAllTasks() {
+    if(!taskList.hasChildNodes()) {
+        errorTask("Não há tarefas");
+        return;
     }
+
+    taskNumber = 1;
+    tasks = [];
+    //addInLocalStorage(tasks);
+    
+    while(taskList.lastChild){
+        taskList.lastChild.remove()
+    }
+
+    UpdateRemainingTasks()
 }
 
 function UpdateRemainingTasks(){
@@ -74,9 +106,31 @@ function UpdateRemainingTasks(){
     let checkedTasks = 0;
 
     for(i = 0; i < checkboxes.length; i++){
+
         if(checkboxes[i].checked){
+            tasks[i].checked = true;
             checkedTasks++;
+            continue;
         }
+
+        tasks[i].checked = false;
     }
+
+    //addInLocalStorage(tasks);
     remainingTasks.textContent = `Your remaining todos: ${taskList.childElementCount - checkedTasks}`
+}
+
+function errorTask(err) {
+    const error = document.querySelector("#err");
+    error.classList.remove("hidden");
+    error.textContent = err;
+}
+
+function clearErr() {
+    const error = document.querySelector("#err");
+    error.classList.add("hidden");
+}
+
+function addInLocalStorage(tasks){
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
