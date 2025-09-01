c34f06bc72ae5c98590169f0f07c5fdb83088569
caio pacely <caiopacely14@gmail.com>
2025-09-01 11:01:32 -0300

    reload LocalStorage

--- script/main.js
+++ script/main.js
@@ -20,6 +20,60 @@ if(localStorage.getItem("tasks") != undefined){
     tasks = JSON.parse(localStorage.getItem("tasks"));
 }
 
+// if (performance.navigation.type === performance.navigation.TYPE_RELOAD ) {
+//     attFromStorage()
+// }
+
+// window.addEventListener("DOMContentLoaded", attFromStorage)
+
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", attFromStorage);
+} else {
+    attFromStorage();
+}
+
+
+function attFromStorage(){
+   const saved = localStorage.getItem("tasks");
+   if (saved) {
+     tasks = JSON.parse(saved);
+     taskList.innerHTML = "";
+     loadInFront()
+     taskNumber = tasks.length + 1;
+     UpdateRemainingTasks();
+   }
+}
+
+function loadInFront(){
+    tasks.forEach((tarefa, index) => {
+      let novaTarefa = document.createElement("li");
+      novaTarefa.id = `${index}`;
+      novaTarefa.className = "main_list__task";
+      novaTarefa.innerHTML =  `
+                           <label class="main_list_task__label">
+                                <input class="label__do" type="checkbox" data-js-checkbox ${tarefa.checked ? "checked" : ""}>
+                                <span class="label__text_task">${tarefa.texto}</span>
+                            </label>
+                             <button id="edit_${index}" class="edit_task" aria-label="edit task" data-js-edit-task-button>
+                                <img src="assets/edit-text.png" alt="edit">
+                            </button>
+                            <button id="${index}" class="delete_task" aria-label="Delete task" data-js-delete-task-button>
+                                <img src="assets/cancel.png" alt="delete">
+                            </button>
+                            `
+
+      const checkbox = novaTarefa.querySelector("[data-js-checkbox]")
+      checkbox.addEventListener("click",UpdateRemainingTasks)
+      taskList.appendChild(novaTarefa);
+
+      const deleteTaskButton = novaTarefa.querySelector("[data-js-delete-task-button]");
+      deleteTaskButton.addEventListener("click", () =>
+      RemoveTask(novaTarefa)
+      );
+      taskList.appendChild(novaTarefa);
+    });
+}
+
 function count_caracter() {
     const caracter = document.querySelector("#input_counter");
     let maxChar = 100;
@@ -92,7 +146,7 @@ function AddTask() {
         checked: false
     });
 
-    //addInLocalStorage(tasks);
+    addInLocalStorage(tasks);
 
     UpdateRemainingTasks()
     taskNumber++;
@@ -101,7 +155,7 @@ function AddTask() {
 function RemoveTask(task) {
     task.remove();
     tasks.slice(task.id, task.id)
-    //addInLocalStorage(tasks);
+    removeTask(task.id)
 
     taskNumber--;
     UpdateRemainingTasks()
@@ -115,7 +169,7 @@ function RemoveAllTasks() {
 
     taskNumber = 1;
     tasks = [];
-    //addInLocalStorage(tasks);
+    addInLocalStorage(tasks);
     
     while(taskList.lastChild){
         taskList.lastChild.remove()
@@ -144,7 +198,7 @@ function UpdateRemainingTasks(){
         clearErr();
     }
 
-    //addInLocalStorage(tasks);
+    addInLocalStorage(tasks);
     remainingTasks.textContent = `Tarefas restantes: ${taskList.childElementCount - checkedTasks}`
 }
 
@@ -161,4 +215,17 @@ function clearErr() {
 
 function addInLocalStorage(tasks){
     localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function RemoveTask(task) {
+  const index = Array.from(taskList.children).indexOf(task);
+
+  if (index > -1) {
+    tasks.splice(index, 1);
+    addInLocalStorage(tasks);
+  }
+  task.remove();
+  taskNumber = taskList.childElementCount + 1;
+
+  UpdateRemainingTasks();
 }
